0b4621d47cccb4a70bb8e631d0f41399ad0433f3
EthTS <ethmain2015@gmail.com>
2024-01-13 00:25:59 +0300

    add Production server to swagger docs

--- src/utility/swagger/config/options.ts
+++ src/utility/swagger/config/options.ts
@@ -5,5 +5,6 @@ export const options = new DocumentBuilder()
     .setDescription('Your API description')
     .setVersion('1.0')
     .addServer('http://localhost:3000/api', 'Local environment')
+    .addServer('https://nest-ecommerce-app.onrender.com/api', "Production environment")
     .addBearerAuth()
     .build();
