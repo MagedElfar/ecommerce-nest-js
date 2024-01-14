60619a74a60b0549a80fb71af7f54570426c7175
EthTS <ethmain2015@gmail.com>
2024-01-14 19:20:26 +0300

    update swagger  description

--- src/utility/swagger/config/options.ts
+++ src/utility/swagger/config/options.ts
@@ -1,8 +1,8 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export const options = new DocumentBuilder()
-    .setTitle('Ecommerce API')
-    .setDescription('Your API description')
+    .setTitle('ECommerce API')
+    .setDescription('Our ECommerce API is designed to empower online businesses by providing a robust and flexible solution for managing products with dynamic variations and attributes. This API allows merchants to easily showcase and sell products with diverse specifications.')
     .setVersion('1.0')
     .addServer('https://nest-ecommerce-app.onrender.com/api', "Production environment")
     .addServer('http://localhost:3000/api', 'Local environment')
